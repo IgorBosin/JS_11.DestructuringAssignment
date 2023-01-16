d2d803adfc30a7d465ce168e298acf4d1d19e289
IgorBosin <ibosin@yandex.ru>
2023-01-16 23:04:48 +0300

    Update DestructuringAssignment.js

--- DestructuringAssignment.js
+++ DestructuringAssignment.js
@@ -19,3 +19,23 @@ function topSalary(salaries) {
     return startKey
     
     
+    
+Задача №2. Создание функции, которая возвращает имя самого НИЗКООПЛАЧИВАЕМОГО сотрудника.    
+
+let salaries = {
+    "John": 100,
+    "Pete": 300,
+    "Mary": 250
+};
+
+function topSalary(salaries) {
+    let startVal = Infinity
+    let startKey = null
+
+    for (let [key, value] of Object.entries(salaries)) {
+        if (value < startVal) {
+            startVal = value
+            startKey = key
+        }
+    }
+    return startKey
